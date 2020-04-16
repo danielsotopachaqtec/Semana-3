5c83ff251e287198fc861c4d1511865ae9b21a77
sotopro <dasg.1993@gmail.com>
2020-04-15 20:33:38 -0500

    [taks] - elevate a number with a function

---
+++ Clase 2/task.js
@@ -0,0 +1,18 @@
+// Crear una función que eleve al cuadrado y al cubo un numero enviado como parámetro a dicha función esta debe ser una variable.
+
+var initialNumber = 4;
+
+function elevate(numberToElevate) {
+	// operations
+	console.log(numberToElevate);
+}
+
+elevate(initialNumber);
+
+var initialNumber = 9;
+
+var initialNumber = 12;
+
+var initialNumber = 64;
+
+var initialNumber = 64;
